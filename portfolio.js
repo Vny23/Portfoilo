d2ffea93132fb46a8398fd86edfd491ba7e99dfa
Vny23 <78056036+Vny23@users.noreply.github.com>
2021-08-20 21:10:10 +0530

    Create portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+const projectName = 'Personal portfolio';
+localStorage.setItem('example_project', 'Technical Docs Page');
+
+
+<img  src="">
+  <h1 style="color:#45567d;">Tarique Ejaz</h1>
+   <h3 style="text-align:center; max-width:60%;">"Enthusiastic Developer. He has the ability to make things happen. Has a clear perspective regarding the development process and knows what to do in order to go about"</h3>
+  <p>Full Stack Development Engineer</p>
+   <h5>Agoda Bngkok, Thiland</h5>
